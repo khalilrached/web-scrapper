fa3d7f0eb095b4ca339c496fdf9c3de21fb88e5b
khalilrached <khalilrached058@gmail.com>
2023-04-02 05:34:50 +0100

    first commit

--- get_data.py
+++ get_data.py
@@ -1,11 +1,20 @@
+import sys
+import os
 import time
 from random import random
-
-import scrapy as sc
-import os
-from bs4 import BeautifulSoup
-from bs4.element import Tag
+from re import match
 import requests
+
+try:
+    from bs4 import BeautifulSoup
+    from bs4.element import Tag, PageElement
+except:
+    print("")
+    sys.exit(0)
+
+
+
+
 
 #  nom de l'école, statut (publique ou privée), et qualité d'éducation.
 
@@ -22,11 +31,11 @@ DATA_SOURCE = {
             'class_name': 'view-dom-id-cd0f4dbc1da641a5bfdddc4cb3f76cbb'
         },
         'symbol': {
-            'url': '/fr/tun-primary-school/symbol/{id}',
-            'class_name': 'region region-content'
+            'url': '/fr/tun-primary-school/symbol',
+            'class_name': 'view-dom-id-4c123d63c456936f3ae2acbf0e59216e'
         }
     },
-    'private': 'https://www.ecoles.com.tn/etablissements/primaire?titreville=&ville=All&garderie=All&prepa=All&restaurant=All&transport=All&clubs=All&page={page}'
+    'private': 'https://www.ecoles.com.tn/etablissements/primaire?titreville=&ville=All&garderie=All&prepa=All&restaurant=All&transport=All&clubs=All&page='
 }
 
 
@@ -46,7 +55,7 @@ def get_html_content(url):
     payload = {'key1': 'value1', 'key2': 'value2'}
     response = session.get(url, data=payload)
     # Get the HTML content of the page
-    if response.status_code not in [202, 200]:
+    if response.status_code not in [202, 201, 200]:
         print(f"NotFoundUrl: {url}")
         raise Exception('url not found.')
     return response.text
@@ -60,6 +69,7 @@ def scrap_html_page(base_url, fn_scrape):
 def map_links(base_url):
     def f(link: Tag):
         return f"{base_url}{link.get('href')}"
+
     return f
 
 
@@ -68,34 +78,61 @@ def get_urls():
     #
 
 
-def get_data():
-    # initialize data folder
-    if not os.path.exists('data'):
-        os.mkdir('data')
+def get_data_private_school():
+    index = 0
+    while True:
+        try:
+            # get region list
+            public_root_content = get_html_content(f"{DATA_SOURCE['private']}{index}")
+            public_root_soup = BeautifulSoup(public_root_content, 'html.parser')
+
+            raw_data = public_root_soup.find_all("div", {"class": 'title-content'})
+            for data in raw_data:
+                print(data.getText())
+            index += 1
+        except:
+            break
+
+def get_data_public_school():
     # get region list
     public_root_content = get_html_content(BASE_URL + DATA_SOURCE['public']['root']['url'])
     public_root_soup = BeautifulSoup(public_root_content, 'html.parser')
 
-    temp = list(filter(lambda t: t.get('class') is not None and DATA_SOURCE['public']['root']['class_name'] in t.get('class'), public_root_soup.find_all('div')))
+    temp = public_root_soup.find("div", {"class": DATA_SOURCE['public']['root']['class_name']})
 
     if len(temp) == 0:
         # error
         raise Exception('error')
 
-    region_list = list(map(map_links(BASE_URL), temp[0].find_all('a')))
+    region_list = list(map(map_links(BASE_URL), temp.find_all('a')))
 
     for u in region_list:
-        time.sleep(random()*3)
-        print(u)
-        public_org_content = get_html_content(u)
-        public_org_soup = BeautifulSoup(public_org_content, 'html.parser')
+        index = 0
+        while True:
+            try:
+                public_org_content = get_html_content(u + f"?page={index}")
+                public_org_soup = BeautifulSoup(public_org_content, 'html.parser')
 
-        data = public_org_soup.find_all('table')[0].find_all_next('a')
+                raw_data = public_org_soup.find_all('table')[0].find_all_next('a')
 
-        print(data)
-        #https://tun.databasesets.com/fr/tun-primary-school/organization/%D8%A3%D8%B1%D9%8A%D8%A7%D9%86%D8%A9
-    pass
+                raw_data = list(filter(lambda link: match(r"[0-9]+", link.getText()), raw_data))
+                raw_data = list(map(PageElement.getText, raw_data))
+                for _id in raw_data:
+                    time.sleep(random() * 2)
+                    public_data_content = get_html_content(BASE_URL + DATA_SOURCE['public']['symbol']['url'] + '/' + _id)
+                    public_data_soup = BeautifulSoup(public_data_content, 'html.parser')
+                    data = public_data_soup.find("div", {"class": 'view-content'})
+                    for d in data:
+                        print(d.getText())
+
+                index += 1
+            except:
+                break
 
 
 if __name__ == '__main__':
-    get_data()
+    # initialize data folder
+    if not os.path.exists('data'):
+        os.mkdir('data')
+    # get_data()
+    get_data_private_school()
